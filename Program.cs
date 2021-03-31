02d7349814e7f4956415ae5ca0ec713a3877ca8a
Fatma Zehra Çiftçi <50120064+fatmazehraCiftci@users.noreply.github.com>
2021-03-31 17:21:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace swappingTwoIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer1 = 5;    //two different integer variable is swapping without third variable
+            int integer2 = 4;
+
+            integer1 = integer1 * integer2;
+            integer2 = integer1 / integer2;
+            integer1 = integer1 / integer2;
+
+            Console.WriteLine("First number is "+integer1+"\nSecond number is "+integer2);
+        }
+    }
+}
